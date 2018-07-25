77f4da1082ed21bd58a16e20426c6cfa9e330c58
Pawel Paszki <ppaszki@redhat.com>
2018-07-25 08:55:49 +0100

    first commit

---
+++ src/main/java/rh/appium/maven/App.java
@@ -0,0 +1,13 @@
+package rh.appium.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
